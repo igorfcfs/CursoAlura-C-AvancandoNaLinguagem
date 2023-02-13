013a0ffdbd75937d53d35079519296c4c8698b9b
igorfcfs <igorfcfs@gmail.com>
2023-02-13 19:02:11 -0300

    Escrevendo as Proprias Funcoes

--- Programa/forca.c
+++ Programa/forca.c
@@ -1,50 +1,33 @@
 #include <stdio.h>
 #include <string.h>
+
+void abertura(){
+    printf("*********************\n");
+    printf("*   Jogo de Forca   *\n");
+    printf("*********************\n\n");
+}
+
+void chuta(char chutes[26], int tentativas){
+    char chute;
+    printf("Qual letra? ");
+    scanf(" %c", &chute);
+
+    chutes[tentativas] = chute;
+}
+
 int main(){
-    /*
-    char letra1 = 'O';
-    char letra2 = 'L';
-    char letra3 = 'A';
-    printf("%c%c%c\n", letra1, letra2, letra3);
-    */
-
-    //Arrays
-    /*
-    int notas[10];
-
-    notas[0] = 10;
-    notas[2] = 15;
-    notas[3] = 20;
-    notas[9] = 30;
-    printf("notas %d %d %d\n", notas[0], notas[2], notas[9]);
-    */
-
     char palavraSecreta[20];
-    /*
-    palavraSecreta[0] = 'M';
-    palavraSecreta[1] = 'E';
-    palavraSecreta[2] = 'L';
-    palavraSecreta[3] = 'A';
-    palavraSecreta[4] = 'N';
-    palavraSecreta[5] = 'C';
-    palavraSecreta[6] = 'I';
-    palavraSecreta[7] = 'A';
-    palavraSecreta[8] = '\0';
 
-    printf("%c%c%c%c%c%c%c%c\n", palavraSecreta[0], palavraSecreta[1],
-           palavraSecreta[2], palavraSecreta[3], palavraSecreta[4], palavraSecreta[5],
-           palavraSecreta[6], palavraSecreta[7]);
-    */
     sprintf(palavraSecreta, "MELANCIA");
-    //printf("%s", palavraSecreta);
 
     int acertou = 0;
     int inforcou = 0;
 
     char chutes[26];
     int tentativas = 0;
-    do {
 
+    abertura();
+    do {
         for(int i = 0; i < strlen(palavraSecreta); i++) {
             int achou = 0;
 
@@ -62,22 +45,10 @@ int main(){
             }
         }
         printf("\n");
-
-        char chute;
-        printf("Qual letra? ");
-        scanf(" %c", &chute);
-
-        chutes[tentativas] = chute;
+        chuta(chutes, tentativas);
         tentativas++;
-        /*
-        for(int i = 0; i < strlen(palavraSecreta); i++){
-            printf("Estou vendo a letra %c na posicao %d\n", palavraSecreta[i], i);
-            if(palavraSecreta[i] == chute){
-                printf("A posicao %d tem essa letra\n", i);
-            }
-        }
-        */
-    } while(!acertou && !inforcou);//while(acertou == 0 && inforcou == 0);
+
+    } while(!acertou && !inforcou);
 
     return 0;
 }

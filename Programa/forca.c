02c3071774c9bd1700a0e63ae09f3b0ec57d94e3
igorfcfs <igorfcfs@gmail.com>
2023-02-13 20:30:54 -0300

    Entrada e Saida(I/O)

--- Programa/forca.c
+++ Programa/forca.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include "forca.h"
 
 //variaveis globais
 char palavraSecreta[20];
 char chutes[26];
-int tentativas = 0;
+int chutesDados = 0;
 
 void abertura(){
     printf("*********************\n");
@@ -12,31 +15,18 @@ void abertura(){
     printf("*********************\n\n");
 }
 
-void chuta(/*char chutes[26], int* tentativas*/){
+void chuta(){
     char chute;
     printf("Qual letra? ");
     scanf(" %c", &chute);
 
-    //chutes[*tentativas] = chute;
-    //(*tentativas)++;
-    chutes[tentativas] = chute;
-    tentativas++;
+    chutes[chutesDados] = chute;
+    chutesDados++;
 }
 
-int jaChutou(char letra/*, char chutes[26], int tentativas*/){
-    int achou = 0;
-    for(int j = 0; j < tentativas; j++) {
-        if(chutes[j] == letra) {
-            achou = 1;
-            break;
-        }
-    }
-    return achou;
-}
-
-void desenhaForca(/*char palavraSecreta[20], char chutes[26], int tentativas*/){
+void desenhaForca(){
     for(int i = 0; i < strlen(palavraSecreta); i++) {
-        int achou = jaChutou(palavraSecreta[i]/*, chutes, tentativas*/);
+        int achou = jaChutou(palavraSecreta[i]);
 
         if(achou) {
             printf("%c ", palavraSecreta[i]);
@@ -47,26 +37,99 @@ void desenhaForca(/*char palavraSecreta[20], char chutes[26], int tentativas*/){
     printf("\n");
 }
 
-void escolhePalavra(/*char palavraSecreta[26]*/){
-    sprintf(palavraSecreta, "MELANCIA");
+int acertou(){
+    for(int i = 0; i < strlen(palavraSecreta); i++){
+        if(!jaChutou(palavraSecreta[i])){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int enforcou(){
+    int erros = 0;
+    for(int i = 0; i < chutesDados; i++){
+        int existe = 0;
+        for(int j = 0; j < strlen(palavraSecreta); j++){
+            if(chutes[i] == palavraSecreta[j]){
+                existe = 1;
+                break;
+            }
+        }
+        if(!existe) erros++;
+    }
+    return erros >= 5;
+}
+
+void adicionaPalavra(){
+    char quer;
+    printf("Voce deseja adicionar uma nova palavra no jogo (S/N)?");
+    scanf(" %c", &quer);
+    if(quer == 'S'){
+        char novaPalavra[20];
+        printf("Qual a nova palavra? ");
+        scanf("%s", novaPalavra);
+
+        FILE* f;
+        f = fopen("palavras.txt", "r+");
+        if(f == 0){
+            printf("Desculpe, o banco de dados nao disponivel\n\n");
+            exit(1);
+        }
+
+        int quantidade;
+        fscanf(f, "%d", &quantidade);
+        quantidade++;
+
+        fseek(f, 0, SEEK_SET);
+        fprintf(f, "%d", quantidade);
+
+        fseek(f, 0, SEEK_END);
+
+        fprintf(f, "%s", novaPalavra);
+        fclose(f);
+    }
+}
+
+void escolhePalavra(){
+    FILE* f;
+    f = fopen("palavras.txt", "r");
+    if(f == 0){
+        printf("Desculpe, o banco de dados nao disponivel\n\n");
+        exit(1);
+    }
+
+    int quantidadeDePalavras;
+    fscanf(f, "%d", &quantidadeDePalavras);
+
+    srand(time(0));
+    int randomico = rand() % quantidadeDePalavras;
+
+    for(int i = 0; i <= randomico; i++){
+        fscanf(f, "%s", palavraSecreta);
+    }
+    fclose(f);
+}
+
+int jaChutou(char letra){
+    int achou = 0;
+    for(int j = 0; j < chutesDados; j++) {
+        if(chutes[j] == letra) {
+            achou = 1;
+            break;
+        }
+    }
+    return achou;
 }
 
 int main(){
-    //char palavraSecreta[20];
-
-    int acertou = 0;
-    int inforcou = 0;
-
-    //char chutes[26];
-    //int tentativas = 0;
-
-    escolhePalavra(/*palavraSecreta*/);
+    escolhePalavra();
     abertura();
     do {
-        desenhaForca(/*palavraSecreta, chutes, tentativas*/);
-        chuta(/*chutes, &tentativas*/);
+        desenhaForca();
+        chuta();
 
-    } while(!acertou && !inforcou);
-
+    } while(!acertou() && !enforcou());
+    adicionaPalavra();
     return 0;
 }
---
+++ Programa/forca.h
@@ -0,0 +1,9 @@
+void abertura();
+void chuta();
+void desenhaForca();
+int acertou();
+int enforcou();
+void adicionaPalavra();
+void escolhePalavra();
+int jaChutou(char letra);
+
